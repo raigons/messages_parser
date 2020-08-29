0eb18e6d70d8c3ce78ba7b857856435c1f475661
Ramon Gonçalves <raigons@gmail.com>
2020-08-29 02:30:56 -0300

    Adds more tests for multiline messages

--- test/integration/reader/message_reader_test.exs
+++ test/integration/reader/message_reader_test.exs
@@ -91,7 +91,68 @@ defmodule Reader.MessageReaderTest do
     test "splits one multiline messages into one record per line with same datetime" do
       file_name = "test/fixtures/sample_4.txt"
 
+      ramon_messages = [
+        %Message{
+          author: "Ramon",
+          datetime: ~N[2018-05-15 09:18:43],
+          content:
+            "Galera hoje esta rolando a votação da lei para legalização do cultivo caseiro de cannabis no Brasil para uso terapêutico e pessoal !!!"
+        },
+        %Message{
+          author: "Ramon",
+          datetime: ~N[2018-05-15 09:18:43],
+          content:
+            "Vamos apoiar a causa , corre lá , faz seu cadastro , confirma o e-mail e vota vota vota !!!!!!!"
+        },
+        %Message{
+          author: "Ramon",
+          datetime: ~N[2018-05-15 09:18:43],
+          content: "💚💚💚💚💚💚💚💚💚💚💚💚💚💚💚💚💚💚💚💚💚🍀🍀🍀🍀🍀🍀🍀🍀🍀🍀🍀🍀"
+        },
+        %Message{
+          author: "Ramon",
+          datetime: ~N[2018-05-15 09:18:43],
+          content: "A união faz a força !!!!!"
+        },
+        %Message{
+          author: "Ramon",
+          datetime: ~N[2018-05-15 09:18:43],
+          content: "https://www12.senado.leg.br/ecidadania/visualizacaomateria?id=132047"
+        },
+        %Message{
+          author: "Ramon",
+          datetime: ~N[2018-05-15 09:18:43],
+          content: "Não deixem de votar !!! E muito importante mesmo !!!!!!!!!!"
+        }
+      ]
+
       {:ok, record} = MessageReader.import(file_name)
+
+      assert %{
+               "Ramon" => ramon_messages
+             } == Agent.get(record, fn messages -> messages end)
+    end
+
+    test "parses multiline messages inside a list of messages and keep parsing normally after" do
+      file_name = "test/fixtures/sample_5.txt"
+
+      john_messages = [
+        %Message{
+          author: "John Doe",
+          datetime: ~N[2018-05-15 08:43:11],
+          content: "Hello ma friend!"
+        },
+        %Message{
+          author: "John Doe",
+          datetime: ~N[2018-05-15 09:34:13],
+          content: "fuckin 4:20"
+        },
+        %Message{
+          author: "John Doe",
+          datetime: ~N[2018-05-15 09:34:14],
+          content: "ouch"
+        }
+      ]
 
       ramon_messages = [
         %Message{
@@ -128,7 +189,10 @@ defmodule Reader.MessageReaderTest do
         }
       ]
 
+      {:ok, record} = MessageReader.import(file_name)
+
       assert %{
+               "John Doe" => john_messages,
                "Ramon" => ramon_messages
              } == Agent.get(record, fn messages -> messages end)
     end
