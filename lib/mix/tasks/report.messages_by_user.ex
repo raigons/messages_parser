f06e7fc6ffb617c1e6ef8b7c489edccc7c6671ca
Ramon Gonçalves <raigons@gmail.com>
2020-09-07 21:18:31 -0300

    Creates mix task for the first report

---
+++ lib/mix/tasks/report.messages_by_user.ex
@@ -0,0 +1,13 @@
+defmodule Mix.Tasks.Report.MessagesByUser do
+  use Mix.Task
+
+  def run(file_path) do
+    report(file_path, File.exists?(file_path))
+  end
+
+  defp report(_, false), do: Mix.shell().error("File does not exist")
+  defp report(file_path, true) do
+    {:ok, record} = Reader.MessageReader.import(file_path)
+    IO.inspect Report.MessagesByUser.count(record)
+  end
+end
