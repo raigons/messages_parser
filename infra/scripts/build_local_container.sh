8ba22d8596aea8c86be9211fd07b2e9184d3cb5e
Ramon Gonçalves <raigons@gmail.com>
2020-09-10 00:05:52 -0300

    Removes pre-existent container before running new one

--- infra/scripts/build_local_container.sh
+++ infra/scripts/build_local_container.sh
@@ -5,4 +5,15 @@ source ./infra/scripts/build_image.sh
 image_name="messages_parser"
 container_name="messages_parser"
 
+if [ "$(docker ps -aq -f status=exited -f name=$container_name)" ]; then
+    # cleanup
+    echo "\n::removing existing container before rebuilding...::\n"
+    docker rm $container_name
+fi
+
+if [ "$(docker ps -aq -f status=running)" ]; then
+    echo "\n::killing and removing existing container before rebuilding...::\n"
+    docker kill $container_name && docker rm $container_name
+fi
+
 docker run --name=$container_name -it -d $image_name sleep infinity
